adc51d51f283a708207c8b2e1e22770b8bd4058f
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-01-29 23:13:27 -0600

    Update Queries.sql
    sample

--- sql/Oracle/Queries.sql
+++ sql/Oracle/Queries.sql
@@ -1,1 +1,11 @@
+/* see all tables */
+select * from all_tables;
 
+/* Data Schema Tables */
+select * from all_tables where owner='DATA_23';
+
+/* Rules Schema Tables */
+select * from all_tables where owner='RULES_23';
+
+/* Customer Schema Tables */
+select * from all_tables where owner='CUSTOMER_23';
